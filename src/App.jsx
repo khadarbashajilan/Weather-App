cd3a0f7d98dba7292b24704dce3cd5aec0e0b371
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-05-14 17:29:18 +0530

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Searchbar from "./components/Searchbar"
 function App() {
   let [data, setdata] = useState({name: null, temp : null, min_temp: null, max_temp: null, feels: null, humidity: null, icon:null});
   let [error, seterror] = useState("");
-  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "36706153ad1f8080a2e40416568f8cba";
+  
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
   const fetchWeather=async(city)=>{
     try{
       const response = await fetch(`${API_URL}?q=${city}&APPID=${API_KEY}&units=metric`);
